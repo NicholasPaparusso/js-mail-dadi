689fdf1bd02848b5a7769b38ccc82e9bf26f5097
NicholasPaparusso <nicopaparusso@gmail.com>
2022-10-14 18:30:58 +0200

    [end]

--- e-mail/js/script.js
+++ e-mail/js/script.js
@@ -1,8 +1,31 @@
-const list = ['piergiorgetto_farabutti@email.it', 'lola97@email.it','manfredonio_devil_angel91@email.it', '50penny@email.uk', 'emo_nemo_dark_rainbow17@email.it' ];
-
+const emailList = ['piergiorgetto_farabutti@email.it', 'lola97@email.it','manfredonio_devil_angel91@email.it', '50penny@email.uk', 'emo_nemo_dark_rainbow17@email.it' ];
+const psw =['expsw']
 const openMenu = document.querySelector('#open-login');
 const closeMenu = document.querySelector('#close');
 const loginMenu = document.querySelector('.login-menu');
+const sendInput = document.getElementById('btn')
+
+
+
+
+sendInput.addEventListener('click', check);
+function check(){
+  const givenEmail = document.getElementById('email').value;
+  const pswGiven = document.getElementById('psw').value;
+  console.log(pswGiven);
+  console.log(givenEmail);
+
+    if(emailList.indexOf(givenEmail) < 0) {
+      alert ('email non trovata');
+    }
+   if (psw.indexOf(pswGiven) < 0) {
+      alert ('password errata');
+    }
+    if (!(emailList.indexOf(givenEmail)<0 && psw.indexOf(pswGiven) < 0)){
+      alert ('accesso effetuato')
+    }
+  }
+
 
 openMenu.addEventListener('click', open);
 function open(){
